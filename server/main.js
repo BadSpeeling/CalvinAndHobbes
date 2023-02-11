4f7a3e1acae699d33bcd2d5f582db016cb892bc8
Eric Frye <eric.frye25@gmail.com>
2023-02-11 18:22:28 -0500

    root server location load html homepage

--- server/main.js
+++ server/main.js
@@ -1,14 +1,27 @@
 const http = require('http');
+const fs = require('fs').promises;
+
+__dirname = "D:/CalvinAndHobbes/"
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<div>Hi</div><hr/><div>Hi</div>');
-});
+const site = (req, res) => {
+  homepage = fs.readFile(__dirname + "/index.html")
+  .then((contents) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(contents);
+  })
+  .catch((err) => {
+
+  }); 
+
+}
+
+const server = http.createServer(site);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
